579557dcf7e5efa95c508a7b321059913afab62a
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-03-17 17:12:27 +0400

    Revert "feat: gemini"
    This reverts commit 47b471671f2927623d54255b7e124bfb72ca5a20.

--- scripts/newDesign.js
+++ scripts/newDesign.js
@@ -1,31 +1,8 @@
 const uiSwitch = document.getElementById('ui')
 const apiConfirm = document.getElementById('apiConfirm')
-const ai = document.getElementById('ai')
-const verAiChoice = document.getElementById('ver')
-const asSwitch = document.querySelector('.ai-switch')
 
 apiConfirm.addEventListener('click', () => {
     if (uiSwitch.checked) {
         document.body.classList.add('newDesign')
-    } else {
-        document.body.classList.remove('newDesign')
-    }
-})
-
-asSwitch.addEventListener('click', () => {
-    if (ai.hasAttribute('src')) {
-        ai.removeAttribute('src')
-
-        if (verAiChoice.checked) {
-            ai.setAttribute('src', 'scripts/geminiTest.js')
-        } else {
-            ai.setAttribute('src', 'scripts/index.js')
-        }
-    }
-    
-    if (verAiChoice.checked) {
-        ai.setAttribute('src', 'scripts/geminiTest.js')
-    } else {
-        ai.setAttribute('src', 'scripts/index.js')
     }
 })
