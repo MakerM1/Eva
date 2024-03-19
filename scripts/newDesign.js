19460b30c1f0b6dfb4fb1563f022621d991e4b58
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-03-19 20:03:27 +0400

    feat: new UI only

--- scripts/newDesign.js
+++
@@ -1,8 +0,0 @@
-const uiSwitch = document.getElementById('ui')
-const apiConfirm = document.getElementById('apiConfirm')
-
-apiConfirm.addEventListener('click', () => {
-    if (uiSwitch.checked) {
-        document.body.classList.add('newDesign')
-    }
-})
